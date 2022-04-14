5e3c1377af4fb0960f8d545cd4db132d0e9c7544
aaronrs2002 <aaronrs2002@yahoo.com>
2022-04-13 22:47:34 -0700

    Add files via upload
    uploaded lesson 2

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Cart from "./components/Cart"
+import PurchaseLog from './components/PurchaseLog';
+
+function App() {
+
+  return (
+    <div className="container">
+
+      <Cart />
+      <PurchaseLog />
+    </div>
+  );
+}
+
+export default App;
